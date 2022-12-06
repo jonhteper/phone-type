977df7d1cb0fd0ddafb9605044169acaff3f3bdd
jonhteper <jonhteper@protonmail.com>
2022-12-06 17:05:36 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cellphone-type
-Cellpone type for Rust
+# Phone-type
+
+This crate contains the `Phone` type, who is only a String wrapper and uses 
+[phone-number-verifier](https://crates.io/crates/phone-number-verifier) to valid the 
+phone's format.
+
+Example
+```rust
+use phone_type::*;
+
+let phone_result = Phone::new("111-111-1111");
+assert!(phone_result.is_ok());
+
+let phone_result = Phone::new_with_country("+52 111 111 1111");
+assert!(phone_result.is_ok(), "Invalid phone with country code");
+ ```
---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+//! Phone type for Rust.
+
+use phone_number_verifier::{
+    verify_phone_number_with_country_code, verify_phone_number_without_country_code,
+};
+use std::fmt::{Display, Formatter};
+
+pub struct ErrorInvalidPhone;
+
+impl Display for ErrorInvalidPhone {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: Invalid phone format")
+    }
+}
+
+pub struct Phone(String);
+
+impl Phone {
+    pub fn new(phone: &str) -> Result<Self, ErrorInvalidPhone> {
+        if !verify_phone_number_without_country_code(phone) {
+            return Err(ErrorInvalidPhone);
+        }
+
+        Ok(Self(phone.to_string()))
+    }
+
+    pub fn new_with_country(phone: &str) -> Result<Self, ErrorInvalidPhone> {
+        if !verify_phone_number_with_country_code(phone) {
+            return Err(ErrorInvalidPhone);
+        }
+
+        Ok(Self(phone.to_string()))
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Display for Phone {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructor_works() {
+        let phone_result = Phone::new("111-111-1111");
+        assert!(phone_result.is_ok(), "Invalid generic phone");
+
+        let phone_result = Phone::new_with_country("+52 111 111 1111");
+        assert!(phone_result.is_ok(), "Invalid phone with country code");
+    }
+}
